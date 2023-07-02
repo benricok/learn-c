e101148725f7ac917588c25447a482ed316c0087
Benrico Krog <benricok@krog.dev>
2023-07-02 21:33:22 +0200

    new file:   act1/main.c

---
+++ act1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    const char NAME[] = "John";
+    int age;
+    const double AVERAGE = 82.4;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Hello, this is %s\n", NAME);
+    printf("I'm %d years old\n", age);
+    printf("My final average was %f\n", AVERAGE);
+
+    age = 22;
+    printf("%s %d %f\n", NAME, age, AVERAGE);
+
+    printf("%f\n", 6.0 / 5 * 4);
+
+    printf("%f\n", pow(2,3));
+    printf("%f\n", sqrt(36));
+    printf("%f\n", floor(3.4));
+    printf("%f\n", ceil(3.4));
+    
+    return 0;
+}
