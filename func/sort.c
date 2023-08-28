d31d9039c086bbf7f261e1f1255721d3b5fe1fec
Benrico Krog <benricok@krog.dev>
2023-08-28 11:16:05 +0200

    update

--- func/sort.c
+++ func/sort.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int arr[6]= {12,5,10,9,7,6};
+    int arr[6]= { 12, 5, 10, 9, 7, 6};
     int temp;
     
-    for(int i=0; i<6; i++) {
+    for(int i=0; i < 6; i++) {
         printf("%d ",arr[i]);
     }
     
-    for(int i=0; i<6; i++) {
-        for(int j=i+1; j<6; j++) { 
+    for(int i=0; i < 6; i++) {
+        for(int j = i+1; j < 6; j++) { 
             if(arr[i]>arr[j]) {
                 temp = arr[i];
                 arr[i] = arr[j];
@@ -18,7 +18,7 @@ int main()
         }
     }
     printf("\n\n");
-    for(int i=0; i<6; i++) {
+    for(int i=0; i < 6; i++) {
         printf("%d ",arr[i]);
     }
 
